3287ee8368bbb9f7a2dc6c51364ddfc81d5197c8
MMikhail1 <aprrp32@gmail.com>
2022-08-27 10:55:54 +0600

    temperature check basic code 1

--- src/test/java/class01/gitBasics.java
+++ src/test/java/class01/gitBasics.java
@@ -2,6 +2,6 @@ package class01;
 
 public class gitBasics {
     public static void main(String[] args) {
-        System.out.println("today is a beautiful day");
+        System.out.println("today is not a beautiful day");
     }
 }
