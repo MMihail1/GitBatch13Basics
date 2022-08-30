d6e3b033bf307bc15f09c44705fc0177def7581b
MMikhail1 <aprrp32@gmail.com>
2022-08-30 07:35:08 +0600

    three lines of additional code

--- src/test/java/class01/gitBasics.java
+++ src/test/java/class01/gitBasics.java
@@ -1,7 +1,10 @@
 package class01;
-
 public class gitBasics {
     public static void main(String[] args) {
+        //write down a code that checks the temperature from a website and then
+        //prints out that it is a beautiful day or not
+        System.out.println("today is not a beautiful day");
+        System.out.println("today is not a beautiful day");
         System.out.println("today is not a beautiful day");
     }
 }
